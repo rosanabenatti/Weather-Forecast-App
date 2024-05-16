e1a22403be966fa3d487304a6b9899d2e1dd8b32
rosanabenatti <rosanabenatti@gmail.com>
2024-05-16 13:57:05 +0200

    Added basic search engine no api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("search-form-input");
-  console.log(searchInput.value);
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
+
 searchFormElement.addEventListener("submit", handleSearchSubmit);
